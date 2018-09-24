77eee11db094fbc8f7e3630e7eb1577b9ddd2e9a
uanoob <usnet@ukr.net>
2018-09-24 10:32:06 +0300

    Allocate store to separate file -> store.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import store from './store/store';
+
 import './App.css';
-import reducers from './store/reducers';
 import UserForm from './UI/UserForm/userForm';
 import UserList from './UI/UserList/userList';
-
-const middleware = applyMiddleware(promiseMiddleware);
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducers, composeWithDevTools(middleware))}>
+      <Provider store={store}>
         <div className="container">
           <UserForm />
           <UserList />
